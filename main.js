a3ce6c05dcdcd589d1aaed497be834aff4c18965
Łukasz Szarowski <lukas.szarkowski@gmail.com>
2020-05-17 21:00:37 +0200

    remove img

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
-alert('Świeżak Siara wita!');
-const firstName='Łukasz';
-const age=32;
+alert("Świeżak Siara wita!");
+const firstName = "Łukasz";
+const age = 32;
 
-
-console.log('Witaj. Mam na imię Łukasz i mam 39 lat')
+console.log("Witaj. Mam na imię Łukasz i mam 39 lat");
