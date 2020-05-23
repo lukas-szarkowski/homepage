94a1e53727609036a0045bccb3a5286d2783cefb
Łukasz Szarowski <lukas.szarkowski@gmail.com>
2020-05-23 22:12:59 +0200

    change navigation

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
 alert("Świeżak Siara wita!");
 const firstName = "Łukasz";
-const age = 32;
+const age = 39;
 
 const heading = document.querySelector(".main__heading--js");
+heading.innerHTML = `Witaj Drogi Odwiedzający, nazywam się ${firstName} i mam ${age}lata.`;
+
 
 console.log("Witaj. Mam na imię Łukasz i mam 39 lat");
 
+function greet (age, firstName) {
+    console.log(
+        `Witaj drogi odwiedzający,nazywam się ${firstName} i mam ${age} lata.`
+    );
+}
+
+greet (age, firstName)
 
