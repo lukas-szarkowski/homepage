271075afbc94f69b93f52ebed246227550f24952
Łukasz Szarowski <lukas.szarkowski@gmail.com>
2020-05-24 16:35:44 +0200

    html folder add

--- main.js
+++ main.js
@@ -3,14 +3,14 @@ const firstName = "Łukasz";
 const age = 39;
 
 const heading = document.querySelector(".main__heading--js");
-heading.innerHTML = `Witaj Drogi Odwiedzający, nazywam się ${firstName} i mam ${age}lata.`;
+heading.innerHTML = `Witaj Drogi Odwiedzający, nazywam się ${firstName} i mam ${age}lat.`;
 
 
 console.log("Witaj. Mam na imię Łukasz i mam 39 lat");
 
 function greet (age, firstName) {
     console.log(
-        `Witaj drogi odwiedzający,nazywam się ${firstName} i mam ${age} lata.`
+        `Witaj drogi odwiedzający,nazywam się ${firstName} i mam ${age} lat.`
     );
 }
 
