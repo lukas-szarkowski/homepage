386fd5fe68337664f0e1767dded900bb676d2ec8
Łukasz Szarowski <lukas.szarkowski@gmail.com>
2020-06-03 22:26:49 +0200

    initial sass scss

--- main.js
+++ main.js
@@ -5,14 +5,29 @@ const age = 39;
 const heading = document.querySelector(".main__heading--js");
 heading.innerHTML = `Witaj Drogi Odwiedzający, nazywam się ${firstName} i mam ${age} lat.`;
 
-
 console.log("Witaj. Mam na imię Łukasz i mam 39 lat");
 
-function greet (age, firstName) {
-    console.log(
-        `Witaj drogi odwiedzający,nazywam się ${firstName} i mam ${age} lat.`
-    );
+function greet(age, firstName) {
+  console.log(
+    `Witaj drogi odwiedzający,nazywam się ${firstName} i mam ${age} lat.`
+  );
 }
 
-greet (age, firstName)
+greet(age, firstName);
 
+const button = document.querySelector(".action--js");
+
+console.log(button);
+
+const myClick = () => {
+  const heading = document.querySelector(".main__heading--js");
+  heading.innerHTML = `Witaj Drogi Odwiedzający, nazywam się ${firstName} i mam ${age} lat.`;
+};
+
+button.addEventListener("click", myClick);
+
+const hamburger = document.querySelector(".hamburger--js");
+hamburger.addEventListener("click", () => {
+  const nav = document.querySelector("navigation--js");
+  nav.classList.toggle("navigation--open");
+});
